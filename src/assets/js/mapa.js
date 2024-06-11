2fdd538e78fcc1ef5c0e8bc4a701a182cbe73b50
Mateo <cuellamateo@gmail.com>
2024-06-11 16:29:01 -0300

    correcion de rutas (incompleto)

--- src/assets/js/mapa.js
+++ src/assets/js/mapa.js
@@ -1,17 +1,17 @@
 
-    const map = L.map('map').setView([-26.1849, -58.1731], 13); // Coordenadas para Formosa, Argentina
+const map = L.map('map').setView([-26.1849, -58.1731], 13); // Coordenadas para Formosa, Argentina
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 
 
-fetch("./estaciones.json").then( datos => datos.json()).then( res => res.forEach( data => {
+fetch("/src/assets/json/estaciones.json").then(datos => datos.json()).then(res => res.forEach(data => {
     L.marker([data.latitude, data.longitude]).addTo(map)
-    .bindPopup(data.name)
-    .openPopup();
+        .bindPopup(data.name)
+        .openPopup();
 
     console.log(data);
 }))
